91fc7e1ef6d63010ea15c35011d8d1b8a1739bc6
emmycruzz7 <emmysants07@gmail.com>
2024-08-28 18:56:33 -0300

    Initial commit

---
+++ info.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP INFO</title>
+</head>
+<body>
+    <?php
+    
+    $nome = "emmy";
+    $altura = 1.64;
+    $idade = 19;
+    $peso = 58.8;
+
+    echo "$nome tem $altura m, sua idade é $idade e pesa $peso.<br>";
+
+    $obj_01 = 4;
+    $obj_02 =  1;
+    print $obj_01 + $obj_02; #imprime o valor 5 
+
+    
+    $linguagem = "PHP";
+
+    echo "Programar em $linguagem é muito legal! <br>";
+    echo 'Programar em $linguagem é muito legal <>br';
+  ?>
+</body>
+</html>
